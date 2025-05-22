58ddbfa5ca83414ff14ea1c910648041571804f5
Huy Nguyen <huy.nguyen@confluent.io>
2025-05-22 16:37:43 -0500

    Adding stop script to teardown k3d

---
+++ stop.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+REQUIRED_PKG="k3d helm kubectl"
+set -o allexport; source .env; set +o allexport
+
+# check for prerequisites
+for PKG in $REQUIRED_PKG; do
+    if [ -z "$(which ${PKG})" ]; then
+        printf "REQUIRED: %s" "${PKG}"
+        printf "\nPlease install %s" "${PKG}"
+        printf "\nUsing Brew:"
+        printf "\n\tbrew install %s" "${PKG}"
+        exit 1
+    fi
+done
+
+# delete k3d
+printf "\nDeleting K3D Cluster......."
+k3d cluster delete $K3D_CLUSTER_NAME
