d972a763725840c5247f0f154cd714334de78ef0
Huy Nguyen <huy.nguyen@confluent.io>
2025-12-26 16:48:24 -0600

    chore: updated bash generation script to allow for specifying kubectl|oc and namespace

--- scripts/helper/generate-ssl-scripts.sh
+++ scripts/helper/generate-ssl-scripts.sh
@@ -1,29 +1,59 @@
 #!/bin/sh
 
 # ./generate-ssl-scripts.sh <component> <gen_dir> <namespace> <fullchain> <privkey> <cacerts>
+# ./generate-ssl-scripts.sh -c <component> -d <gen_dir> -n <namespace> -f <fullchain> -p <privkey> -a <cacerts>
+#
 
 HOME_DIR=$(pwd)
-REQUIRED_PKG="kubectl yq jq"
+# reset
+OPTIND=1
 set -o allexport; source .env; set +o allexport
 
-COMPONENT=$1
-GEN_DIR="$2"
-GEN_DIR_SSL_CMD="$GEN_DIR/cmd"
-NAMESPACE=$3
-FULLCHAIN_PATH=$4
-CACERTS_PATH=$5
-PRIVKEY_PATH=$6
+# flags
+usage () {
+    printf "Usage: $0 [-c] [string] [-d] [path] [-n] [string] [-f] [path] [-p] [path] [-a] [path] \n"
+    printf "\t-c component_name                 (required) component name, e.g. kafka\n"
+    printf "\t-d directory                      (required) output directory, e.g. full path\n"
+    printf "\t-n namespace                      (required) kubernetes namesapce to use in script, e.g. confluent\n"
+    printf "\t-f fullchain.pem                  (required) fullchain PEM file for the component\n"
+    printf "\t-p private.key                    (required) private key in PEM format for the component\n"
+    printf "\t-a cacerts.pem                    (required) cacerts PEM file\n"
+    exit 1
+}
 
-# check for prerequisites
-for PKG in $REQUIRED_PKG; do
-    if [ -z "$(which ${PKG})" ]; then
-        printf "REQUIRED: %s" "${PKG}"
-        printf "\nPlease install %s" "${PKG}"
-        printf "\nUsing Brew:"
-        printf "\n\tbrew install %s" "${PKG}"
-        exit 1
-    fi
+while getopts "c:d:n:f:p:a:" opt; do
+    case $opt in
+        c)
+            COMPONENT=$OPTARG
+            ;;
+        d)
+            GEN_DIR=$OPTARG
+            GEN_DIR_SSL_CMD="$GEN_DIR/cmd"
+            ;;
+        n)
+            NAMESPACE=$OPTARG
+            ;;
+        f)
+            FULLCHAIN_PATH=$OPTARG
+            ;;
+        p)
+            PRIVKEY_PATH=$OPTARG
+            ;;
+        a)
+            CACERTS_PATH=$OPTARG
+            ;;
+        *)
+            usage
+            ;;
+    esac
 done
+
+# ensure input has value
+if [ -z "$COMPONENT" ] || [ -z "$GEN_DIR" ] || [ -z "$NAMESPACE" ] || [ -z "$FULLCHAIN_PATH" ] || [ -z "$CACERTS_PATH" ] || [ -z "$PRIVKEY_PATH" ]; then
+    printf "Error: $0 requires arguments\n"
+    usage
+fi
+
 
 if [ ! -d "$GEN_DIR" ]; then
     printf "\nGenerated Directory Path UNKNOWN...."
@@ -40,11 +70,12 @@ if [[ ! -f "$FULLCHAIN_PATH" ]] || [[ ! -f "$CACERTS_PATH" ]] || [[ ! -f "$PRIVK
 fi
 
 generate_bash_script () {
-
-    cmd="kubectl -n $NAMESPACE create secret generic tls-$COMPONENT --from-file=fullchain.pem=$FULLCHAIN_PATH --from-file=cacerts.pem=$CACERTS_PATH --from-file=privkey.pem=$PRIVKEY_PATH"
+    
+    cmd="-n \$NAMESPACE create secret generic tls-$COMPONENT --from-file=fullchain.pem=$FULLCHAIN_PATH --from-file=cacerts.pem=$CACERTS_PATH --from-file=privkey.pem=$PRIVKEY_PATH"
     file_name="create-tls-$COMPONENT-secret.sh"
     
-    printf "\nKubectl Command: %s\n" "$cmd"
+    # uncomment to debug
+    #printf "\nKubectl Command: eval kubectl|oc %s\n" "$cmd"
 
     # Create cmd direcotry for component
     if [ ! -d "$GEN_DIR_SSL_CMD/$COMPONENT" ]; then
@@ -52,10 +83,14 @@ generate_bash_script () {
         mkdir -p "$GEN_DIR_SSL_CMD/$COMPONENT"
     fi
 
-    printf "#!/bin/sh\n%s\n" "$cmd" > $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
+    printf "#!/bin/sh\n\n" > $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
+    printf "# ./$file_name [kubectl|oc] [namespace]\n\n" >> $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
+    printf "KCMD=\${1:-kubectl}\n" >> $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
+    printf "NAMESPACE=\${2:-%s}\n" "$NAMESPACE" >> $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
+    printf "eval \"\$KCMD %s\"\n" "$cmd" >> $GEN_DIR_SSL_CMD/$COMPONENT/$file_name
     chmod +x "$GEN_DIR_SSL_CMD/$COMPONENT/$file_name"
 
-    printf "\nCreated bash script at %s\n" "$GEN_DIR_SSL_CMD/$COMPONENT/$file_name"
+    printf "Created bash script at %s\n" "$GEN_DIR_SSL_CMD/$COMPONENT/$file_name"
 }
 
 generate_bash_script
