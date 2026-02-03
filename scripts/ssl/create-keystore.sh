922d454895072dcab3d1e7f2e8563c859b274ab7
Huy Nguyen <huy.nguyen@confluent.io>
2026-02-03 16:05:08 -0600

    feat: added fips option to keystore/truststore generation

--- scripts/ssl/create-keystore.sh
+++ scripts/ssl/create-keystore.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-# ./create-keystore.sh "component name" "path_to_private_key" "path_to_fullchain" "keystore_password"
+# ./create-keystore.sh -c "component name" -k "path_to_private_key" -a "path_to_fullchain" -p "keystore_password" -f -x
 
-# TODO: Add option to create BCFKS https://docs.confluent.io/operator/current/co-security-compliance.html
-
-REQUIRED_PKG="openssl keytool"
-GEN_DIR="$BASE_DIR/generated/ssl"
-COMPONENT_NAME=$1
-CERT_KEY=$2
-CERT_PEM=$3
-KEYSTORE_PASSWORD=$4
-KEYSTORE_FILE="$GEN_DIR/files/$COMPONENT_NAME.keystore.p12"
-KEYSTORE_ALIAS="1"
-KEYSTORE_JKS_FILE="$GEN_DIR/files/$COMPONENT_NAME.keystore.jks"
-PASSWORD_FILE="$GEN_DIR/files/$COMPONENT_NAME.keystore.jksPassword.txt"
-
-# Check number of arguments
-if [ "$#" -ne 4 ]; then
-    printf "Command takes 4 arguments, exiting...\n\n"
-    printf "./create-keystore.sh \"component name\" \"path_to_private_key\" \"path_to_fullchain\" \"keystore_password\"\n"
-    exit 1
-fi
+REQUIRED_PKG="curl openssl keytool"
+OPTIND=1
+GEN_DIR="$BASE_DIR/generated"
+SSL_DIR="$GEN_DIR/ssl"
+# FIPS 140-2 certified = 1.0.2.* releases until 2026 September
+# FIPS 140-3 certified = 2.*
+BC_VERSION="1.0.2.3"
+BC_JAR="bc-fips-${BC_VERSION}.jar"
+BC_JAR_URL="https://repo1.maven.org/maven2/org/bouncycastle/bc-fips/$BC_VERSION/$BC_JAR"
+FIPS_ENABLED=false
+CLEAN_BUILD=false
 
 # check for prerequisites
 for PKG in $REQUIRED_PKG; do
@@ -33,29 +25,169 @@ for PKG in $REQUIRED_PKG; do
     fi
 done
 
-# create generated directories if missing
-mkdir -p $GEN_DIR/files
-
-# generate p12 with private key and public fullchain, must use -legacy option for OpenSSL 3.x
-rm -f "$KEYSTORE_FILE"
-rm -f "$KEYSTORE_JKS_FILE"
-printf "Create PKCS12 formatted keystore for %s...\n" "$COMPONENT_NAME"
-openssl pkcs12 -export -in $CERT_PEM -inkey $CERT_KEY -out $KEYSTORE_FILE -password "pass:${KEYSTORE_PASSWORD}" > /dev/null 2>&1
-
-# error checking
-if [ ! -f "$KEYSTORE_FILE" ]; then
-    printf "PKCS12 Keystore creation encountered an error...\n"
+# make sure keytool is working
+keytool -h > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+    printf "keytool command not working as expected, exiting...\n"
     exit 1
 fi
 
-# convert to jks
-printf "Converting PKCS12 Formatted Keystore into JKS Formatted Keystore for %s...\n" "$COMPONENT_NAME"
-keytool -importkeystore -srcstorepass $KEYSTORE_PASSWORD -srckeystore $KEYSTORE_FILE -srcstoretype pkcs12 -srcalias $KEYSTORE_ALIAS -destkeystore $KEYSTORE_JKS_FILE -deststoretype jks -deststorepass $KEYSTORE_PASSWORD -destalias $KEYSTORE_ALIAS > /dev/null 2>&1
-
-# error checking
-if [ ! -f "$KEYSTORE_JKS_FILE" ]; then
-    printf "JKS Keystore creation encountered an error...\n"
+# flags
+usage () {
+    printf "Usage: $0 [-c] [string] [-k] [string] [-a] [string] [-p] [string] [-f] [-x]\n"
+    printf "\t-c [component name]                   (required) component name\n"
+    printf "\t-k [path to private key]              (required) path to private key PEM file\n"
+    printf "\t-a [path to fullchain]                (required) path to fullchain PEM file\n"
+    printf "\t-p [keystore password]                (required) keystore password\n"
+    printf "\t-f                                    (optional) Create a Bouncy Castle Keystore\n"
+    printf "\t-x                                    (optional) Clean build\n"
+    printf "\t-h                                    help menu\n"
     exit 1
+}
+
+while getopts "c:k:a:p:fx" opt; do
+    case $opt in
+        c)
+            # component name
+            COMPONENT_NAME=$OPTARG
+            ;;
+        k)
+            # private key
+            CERT_KEY=$OPTARG
+            ;;
+        a)
+            # path to fullchain
+            CERT_PEM=$OPTARG
+            ;;
+        p)
+            # keystore password
+            KEYSTORE_PASSWORD=$OPTARG
+            ;;
+        f)
+            # fips mode enabled
+            FIPS_ENABLED=true
+            ;;
+        x)
+            # clean build will delete existing files
+            CLEAN_BUILD=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# set the default if not set
+if [ -z "$COMPONENT_NAME" ] || [ -z "$CERT_KEY" ] || [ -z "$CERT_PEM" ] || [ -z "$KEYSTORE_PASSWORD" ]; then
+    printf "Parameters must be set!\n"
+    usage
+    exit 1
+fi
+
+KEYSTORE_PKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.keystore.p12"
+KEYSTORE_ALIAS="1"
+KEYSTORE_JKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.keystore.jks"
+PASSWORD_FILE="$SSL_DIR/files/$COMPONENT_NAME.keystore.jksPassword.txt"
+KEYSTORE_BCFKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.keystore.bcfks"
+
+download_bc_jar () {
+
+    if [ ! -f "$GEN_DIR/jars" ]; then
+       mkdir -p $GEN_DIR/jars 
+    fi
+
+    if [ ! -f "$GEN_DIR/jars/$BC_JAR" ]; then
+        curl "$BC_JAR_URL" -o "$GEN_DIR/jars/$BC_JAR"
+    fi
+
+    if [ "$?" -ne 0 ]; then
+        printf "There was an issue downloading the jar, exiting...\n"
+        exit 1
+    fi
+}
+
+create_bc_keystore () {
+
+    # confirm that PKCS12 keystore exists
+    if [ ! -f "$KEYSTORE_PKS_FILE" ]; then
+        printf "PKCS12 keystore doesn't exist, required for conversion, exiting...\n"
+        exit 1
+    fi
+
+    if [ -f "$KEYSTORE_BCFKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "BCFKS keystore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ]; then
+            printf "Deleting existing BCFKS keystore file...\n"
+            rm -f "$KEYSTORE_BCFKS_FILE"
+        fi
+        # generate BCFKS keystore
+        keytool -importkeystore -srcstorepass $KEYSTORE_PASSWORD -srckeystore $KEYSTORE_PKS_FILE -srcstoretype pkcs12 -srcalias $KEYSTORE_ALIAS -destkeystore $KEYSTORE_BCFKS_FILE -deststoretype BCFKS -deststorepass $KEYSTORE_PASSWORD -destalias $KEYSTORE_ALIAS -providername BCFIPS -providerclass org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider -providerpath $GEN_DIR/jars/$BC_JAR > /dev/null 2>&1
+    fi
+
+    # error checking
+    if [ ! -f "$KEYSTORE_BCFKS_FILE" ]; then
+        printf "BCFKS Keystore creation encountered an error...\n"
+        exit 1
+    fi
+
+
+}
+
+create_keystore () {
+
+    if [ -f "$KEYSTORE_PKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "PKCS12 keystore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ]; then
+            printf "Deleting existing PKCS12 keystore file...\n"
+            rm -f "$KEYSTORE_PKS_FILE"
+        fi
+       
+        # generate p12 with private key and public fullchain, must use -legacy option for OpenSSL 3.x
+        printf "Create PKCS12 formatted keystore for %s...\n" "$COMPONENT_NAME"
+        openssl pkcs12 -export -in $CERT_PEM -inkey $CERT_KEY -out $KEYSTORE_PKS_FILE -password "pass:${KEYSTORE_PASSWORD}" > /dev/null 2>&1
+    fi
+
+    # error checking
+    if [ ! -f "$KEYSTORE_PKS_FILE" ]; then
+        printf "PKCS12 Keystore creation encountered an error...\n"
+        exit 1
+    fi
+
+    if [ -f "$KEYSTORE_JKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "JKS keystore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ]; then
+            printf "Deleting existing JKS keystore file...\n"
+            rm -f "$KEYSTORE_JKS_FILE"
+        fi
+
+        # convert to jks
+        printf "Converting PKCS12 Formatted Keystore into JKS Formatted Keystore for %s...\n" "$COMPONENT_NAME"
+        keytool -importkeystore -srcstorepass $KEYSTORE_PASSWORD -srckeystore $KEYSTORE_PKS_FILE -srcstoretype pkcs12 -srcalias $KEYSTORE_ALIAS -destkeystore $KEYSTORE_JKS_FILE -deststoretype jks -deststorepass $KEYSTORE_PASSWORD -destalias $KEYSTORE_ALIAS > /dev/null 2>&1
+    fi
+
+    # error checking
+    if [ ! -f "$KEYSTORE_JKS_FILE" ]; then
+        printf "JKS Keystore creation encountered an error...\n"
+        exit 1
+    fi
+
+
+}
+
+# create generated directories if missing
+mkdir -p $SSL_DIR/files
+
+# call create keystore
+create_keystore
+
+if [ "$FIPS_ENABLED" == true ]; then
+    download_bc_jar
+    
+    # call create bcfks
+    create_bc_keystore
 fi
 
 # generate jksPassword.txt
--- scripts/ssl/create-truststore.sh
+++ scripts/ssl/create-truststore.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# ./create-truststore.sh "component" "path_to_cacerts" "keystore_password"
+# ./create-truststore.sh -c "component" -a "path_to_cacerts" -p "keystore_password" -f -x
 
-# TODO: Add option to create BCFKS https://docs.confluent.io/operator/current/co-security-compliance.html
-
-REQUIRED_PKG="openssl keytool"
-GEN_DIR="$BASE_DIR/generated/ssl"
-COMPONENT_NAME=$1
-CACERTS=$2
-TRUSTSTORE_PASSWORD=$3
-TRUSTSTORE_FILE="$GEN_DIR/files/$COMPONENT_NAME.truststore.p12"
-TRUSTSTORE_JKS_FILE="$GEN_DIR/files/$COMPONENT_NAME.truststore.jks"
+REQUIRED_PKG="curl openssl keytool"
+OPTIND=1
+GEN_DIR="$BASE_DIR/generated"
+SSL_DIR="$GEN_DIR/ssl"
 CA_ALIAS="caroot"
-PASSWORD_FILE="$GEN_DIR/files/$COMPONENT_NAME.truststore.jksPassword.txt"
-
-# Check number of arguments
-if [ "$#" -ne 3 ]; then
-    printf "Command takes 3 arguments, exiting...\n\n"
-    printf "./create-truststore.sh \"component name\" \"path_to_cacerts\" \"keystore_password\"\n"
-    exit 1
-fi
+# FIPS 140-2 certified = 1.0.2.* releases until 2026 September
+# FIPS 140-3 certified = 2.*
+BC_VERSION="1.0.2.3"
+BC_JAR="bc-fips-${BC_VERSION}.jar"
+BC_JAR_URL="https://repo1.maven.org/maven2/org/bouncycastle/bc-fips/$BC_VERSION/$BC_JAR"
+FIPS_ENABLED=false
+CLEAN_BUILD=false
 
 # check for prerequisites
 for PKG in $REQUIRED_PKG; do
@@ -32,34 +26,160 @@ for PKG in $REQUIRED_PKG; do
     fi
 done
 
-# create generated directories if missing
-mkdir -p $GEN_DIR/files
-
-# generate p12 truststore
-rm -f "$TRUSTSTORE_FILE"
-rm -f "$TRUSTSTORE_JKS_FILE"
-printf "Create PKCS12 formatted truststore for %s...\n" "$COMPONENT_NAME"
-openssl pkcs12 -export -nokeys -in $CACERTS -out $TRUSTSTORE_FILE -caname $CA_ALIAS -password "pass:${TRUSTSTORE_PASSWORD}" -jdktrust anyExtendedKeyUsage > /dev/null 2>&1
-
-# error checking
-if [ ! -f "$TRUSTSTORE_FILE" ]; then
-    printf "PKCS12 Truststore creation encountered an error...\n"
+# make sure keytool is working
+keytool -h > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+    printf "keytool command not working as expected, exiting...\n"
     exit 1
 fi
 
-# convert to jks
-printf "Converting PKCS12 Formatted Truststore into JKS Formatted Truststore for %s...\n" "$COMPONENT_NAME"
-# loop through pkcs12 truststore
-keytool -list -keystore $TRUSTSTORE_FILE -storepass $TRUSTSTORE_PASSWORD | grep 'trustedCertEntry' | sed -E "s/^([0-9a-zA-Z]*),.*/\1/g" | while IFS= read -r line; do
-    printf "Importing Certificate %s\n" "$line"
-    keytool -importkeystore -srcstorepass $TRUSTSTORE_PASSWORD -srckeystore $TRUSTSTORE_FILE -srcstoretype pkcs12 -srcalias $line -destkeystore $TRUSTSTORE_JKS_FILE -deststoretype jks -deststorepass $TRUSTSTORE_PASSWORD -destalias $line > /dev/null 2>&1
+# flags
+usage () {
+    printf "Usage: $0 [-c] [string] [-a] [string] [-p] [string] [-f] [-x]\n"
+    printf "\t-c [component name]                   (required) component name\n"
+    printf "\t-a [path to cacerts]                  (required) path to cacerts\n"
+    printf "\t-p [truststore password]              (required) truststore password\n"
+    printf "\t-f                                    (optional) Create a Bouncy Castle Keystore\n"
+    printf "\t-x                                    (optional) Clean build\n"
+    printf "\t-h                                    help menu\n"
+    exit 1
+}
+
+while getopts "c:a:p:fx" opt; do
+    case $opt in
+        c)
+            # component name
+            COMPONENT_NAME=$OPTARG
+            ;;
+        a)
+            # path to cacerts
+            CACERTS=$OPTARG
+            ;;
+        p)
+            # keystore password
+            TRUSTSTORE_PASSWORD=$OPTARG
+            ;;
+        f)
+            # fips mode enabled
+            FIPS_ENABLED=true
+            ;;
+        x)
+            # clean build will delete existing files
+            CLEAN_BUILD=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
 done
 
-# error checking
-if [ ! -f "$TRUSTSTORE_JKS_FILE" ]; then
-    printf "JKS Truststore creation encountered an error...\n"
+# set the default if not set
+if [ -z "$COMPONENT_NAME" ] || [ -z "$CACERTS" ] || [ -z "$TRUSTSTORE_PASSWORD" ]; then
+    printf "Parameters must be set!\n"
+    usage
     exit 1
 fi
+
+# Set file path
+TRUSTSTORE_JKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.truststore.jks"
+TRUSTSTORE_PKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.truststore.p12"
+TRUSTSTORE_BCFKS_FILE="$SSL_DIR/files/$COMPONENT_NAME.truststore.bcfks"
+PASSWORD_FILE="$SSL_DIR/files/$COMPONENT_NAME.truststore.jksPassword.txt"
+
+download_bc_jar () {
+
+    if [ ! -f "$GEN_DIR/jars" ]; then
+       mkdir -p $GEN_DIR/jars 
+    fi
+
+    if [ ! -f "$GEN_DIR/jars/$BC_JAR" ]; then
+        curl "$BC_JAR_URL" -o "$GEN_DIR/jars/$BC_JAR"
+    fi
+
+    if [ "$?" -ne 0]; then
+        printf "There was an issue downloading the jar, exiting...\n"
+        exit 1
+    fi
+}
+
+create_bc_truststore () {
+
+    cmd="keytool -noprompt -keystore $TRUSTSTORE_BCFKS_FILE -storetype BCFKS -alias $CA_ALIAS -import -file $CACERTS -storepass $TRUSTSTORE_PASSWORD -keypass $TRUSTSTORE_PASSWORD -providerclass org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider -providerpath $GEN_DIR/jars/$BC_JAR"
+
+    if [ -f "$TRUSTSTORE_BCFKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "BCFKS truststore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ]; then
+            printf "Deleting existing BCFKS truststore file...\n"
+            rm -f "$TRUSTSTORE_BCFKS_FILE"
+        fi
+        # create truststore
+        printf "Creating BCFKS truststore...\n"
+        printf "\n%s\n" "$cmd"
+        eval $cmd
+    fi
+}
+
+create_truststore () {
+
+    if [ -f "$TRUSTSTORE_PKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "PKCS12 truststore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ]; then
+            printf "Deleting existing PKCS12 truststore file...\n"
+            rm -f "$TRUSTSTORE_PKS_FILE"
+        fi
+
+        # generate pkcs12 truststore
+        printf "Create PKCS12 formatted truststore for %s...\n" "$COMPONENT_NAME"
+        openssl pkcs12 -export -nokeys -in $CACERTS -out $TRUSTSTORE_PKS_FILE -caname $CA_ALIAS -password "pass:${TRUSTSTORE_PASSWORD}" -jdktrust anyExtendedKeyUsage > /dev/null 2>&1
+
+    fi
+
+    # error checking
+    if [ ! -f "$TRUSTSTORE_PKS_FILE" ]; then
+        printf "PKCS12 Truststore creation encountered an error...\n"
+        exit 1
+    fi
+
+    if [ -f "$TRUSTSTORE_JKS_FILE" ] && [ "$CLEAN_BUILD" == false ]; then
+        printf "JKS truststore file exists, skipping...\n"
+    else
+        if [ "$CLEAN_BUILD" == true ];then
+            printf "Deleting existing JKS truststore file...\n"
+            rm -f "$TRUSTSTORE_JKS_FILE"
+        fi
+
+        # convert to jks
+        printf "Converting PKCS12 Formatted Truststore into JKS Formatted Truststore for %s...\n" "$COMPONENT_NAME"
+        # loop through pkcs12 truststore
+        keytool -list -keystore $TRUSTSTORE_PKS_FILE -storepass $TRUSTSTORE_PASSWORD | grep 'trustedCertEntry' | sed -E "s/^([0-9a-zA-Z]*),.*/\1/g" | while IFS= read -r line; do
+        printf "Importing Certificate %s\n" "$line"
+        keytool -importkeystore -srcstorepass $TRUSTSTORE_PASSWORD -srckeystore $TRUSTSTORE_PKS_FILE -srcstoretype pkcs12 -srcalias $line -destkeystore $TRUSTSTORE_JKS_FILE -deststoretype jks -deststorepass $TRUSTSTORE_PASSWORD -destalias $line > /dev/null 2>&1
+        done
+
+    fi
+
+    # error checking
+    if [ ! -f "$TRUSTSTORE_JKS_FILE" ]; then
+        printf "JKS Truststore creation encountered an error...\n"
+        exit 1
+    fi
+
+}
+
+# create generated directories if missing
+mkdir -p $SSL_DIR/files
+
+if [ "$FIPS_ENABLED" == true ]; then
+    download_bc_jar
+    
+    # call create bcfks
+    create_bc_truststore
+fi
+
+# call create truststore
+create_truststore
 
 # generate jksPassword.txt
 printf "Creating jksPassword.txt for %s...\n" "$COMPONENT_NAME"
