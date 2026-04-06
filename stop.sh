1f3d2a59105c821221424c1313705340dea83e61
Huy Nguyen <huy.nguyen@confluent.io>
2026-04-06 15:54:11 -0500

    feat: Improving stop script and added stopping for CRC

--- stop.sh
+++ stop.sh
@@ -3,41 +3,50 @@
 # TODO: Add in stop to shutdown multipass or not
 # TODO: Add teardown options for Terraform and Openshift
 
-REQUIRED_PKG="k3d multipass"
 set -o allexport; source .env; set +o allexport
 
-# check for prerequisites
-for PKG in $REQUIRED_PKG; do
-    if [ -z "$(which ${PKG})" ]; then
-        printf "REQUIRED: %s" "${PKG}"
-        printf "\nPlease install %s" "${PKG}"
-        printf "\nUsing Brew:"
-        printf "\n\tbrew install %s" "${PKG}"
-        exit 1
-    fi
-done
-
 source $BASE_DIR/scripts/system/header.sh -t "Shutting Down Kube Playground"
-# check if k3d is running
-if [ "$(k3d cluster list $K3D_CLUSTER_NAME)" ]; then
-    # delete k3d
-    printf "\nDeleting K3D Cluster.......\n"
-    k3d cluster delete $K3D_CLUSTER_NAME
 
-    exit 0
-else
-    printf "\nNo K3D Cluster named %s Found...\n" "$K3D_CLUSTER_NAME"
+# check if k3d is running
+if [ ! -z "$(which k3d)" ]; then
+    if k3d cluster list $K3D_CLUSTER_NAME > /dev/null 2>&1; then
+        # delete k3d
+        printf "\nDeleting K3D Cluster.......\n"
+        k3d cluster delete $K3D_CLUSTER_NAME
+        exit 0
+    fi
+
+    printf "No k3d Cluster Found...\n"
 fi
 
 # check if multipass is deployed
-if [ "$(multipass list | grep -c $MULTIPASS_VM_NAME)" -ne 0 ]; then
-    # stop multipass vm
-    printf "\nStopping Multipass VM.....\n"
-    multipass stop $MULTIPASS_VM_NAME
-    printf "\nMultipass VM has stopped, you can restart it with\n\tmultipass start %s\n" "$MULTIPASS_VM_NAME"
+if [ ! -z "$(which multipass)" ]; then
+    if [ "$(multipass list | grep -c $MULTIPASS_VM_NAME)" -ne 0 ]; then
+        # stop multipass vm if running
+        if [ "$(multipass list | grep $MULTIPASS_VM_NAME | awk '{print $2}' | grep -ic running)" -ge 1 ]; then
+            printf "\nStopping Multipass VM.....\n"
+            multipass stop $MULTIPASS_VM_NAME
+            printf "\nMultipass VM has stopped, you can restart it with\n\tmultipass start %s\n" "$MULTIPASS_VM_NAME"
+            exit 0
+        fi
+    fi
 
-    exit 0
+    printf "No Multipass VM Found...\n"
 fi
 
-printf "\nNo k3d instance or multipass vm, exiting...\n"
+# check if CRC is running
+if [ ! -z "$(which crc)" ]; then
+    if [ "$(crc status 2>&1 | grep -ic 'crc setup')" -ne 1 ]; then
+        if [ "$(crc status 2>&1 | grep -ic 'crc start')" -eq 1 ]; then
+            printf "CRC found, stopping.....\n"
+            crc stop
+            printf "CRC has stopped, you can restart it with \n\tcrc start\n"
+            exit 0
+        fi
+    fi
+
+    printf "No CRC VM Found...\n"
+fi
+
+printf "\nNothing managed by kube-playground found running, exiting...\n"
 exit 1
