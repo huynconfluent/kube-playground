ecca9f314947913c1466ade1c74a1fca89563cd3
Huy Nguyen <huy.nguyen@confluent.io>
2026-04-07 08:49:35 -0500

    feat: adding a destroy option

--- stop.sh
+++ stop.sh
@@ -3,7 +3,25 @@
 # TODO: Add in stop to shutdown multipass or not
 # TODO: Add teardown options for Terraform and Openshift
 
+DESTROY=false
 set -o allexport; source .env; set +o allexport
+
+usage() {
+    printf "Usage: $0 [-c]\n"
+    printf "\t-d        (optional) delete option to destroy multipass and crc vm\n"
+    exit 1
+}
+
+while getopts "d" opt; do
+    case $opt in
+        d)
+            DESTROY=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
 
 source $BASE_DIR/scripts/system/header.sh -t "Shutting Down Kube Playground"
 
@@ -24,9 +42,17 @@ if [ ! -z "$(which multipass)" ]; then
     if [ "$(multipass list | grep -c $MULTIPASS_VM_NAME)" -ne 0 ]; then
         # stop multipass vm if running
         if [ "$(multipass list | grep $MULTIPASS_VM_NAME | awk '{print $2}' | grep -ic running)" -ge 1 ]; then
-            printf "\nStopping Multipass VM.....\n"
-            multipass stop $MULTIPASS_VM_NAME
-            printf "\nMultipass VM has stopped, you can restart it with\n\tmultipass start %s\n" "$MULTIPASS_VM_NAME"
+            if [ "$DESTROY" == "false" ]; then
+                printf "\nStopping Multipass VM.....\n"
+                multipass stop $MULTIPASS_VM_NAME
+                printf "\nMultipass VM has stopped, you can restart it with\n\tmultipass start %s\n" "$MULTIPASS_VM_NAME"
+            else
+                printf "\nStopping and Destroying the Multipass VM.....\n"
+                multipass stop $MULTIPASS_VM_NAME
+                multipass delete $MULTIPASS_VM_NAME
+                multipass purge
+                printf "Mulitpass VM Deleted!\n"
+            fi
             exit 0
         fi
     fi
@@ -38,9 +64,17 @@ fi
 if [ ! -z "$(which crc)" ]; then
     if [ "$(crc status 2>&1 | grep -ic 'crc setup')" -ne 1 ]; then
         if [ "$(crc status 2>&1 | grep -i 'crc vm:' | grep -ic 'running')" -eq 1 ]; then
-            printf "CRC found, stopping.....\n"
-            crc stop
-            printf "CRC has stopped, you can restart it with \n\tcrc start\n"
+            if [ "$DESTROY" == "false" ]; then
+                printf "CRC found, stopping.....\n"
+                crc stop
+                printf "CRC has stopped, you can restart it with \n\tcrc start\n"
+            else
+                printf "CRC found, stopping and destroying the CRC VM.....\n"
+                crc stop
+                crc delete
+                crc cleanup
+                printf "CRC VM Deleted!\n"
+            fi
             exit 0
         fi
     fi
