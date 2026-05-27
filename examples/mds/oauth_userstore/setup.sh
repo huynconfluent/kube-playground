4fe0ba98b21b2451d372440621d904666bac2355
Huy Nguyen <huy.nguyen@confluent.io>
2026-05-27 11:52:18 -0500

    Adding MDS OAUTH Userstore example

---
+++ examples/mds/oauth_userstore/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+source $BASE_DIR/generated/ssl/cmd/keypair/create-mds-keypair.sh
+source $BASE_DIR/generated/ssl/cmd/kafkacontroller/create-tls-kafkacontroller-secret.sh
+source $BASE_DIR/generated/ssl/cmd/kafkabroker/create-tls-kafkabroker-secret.sh
+source $BASE_DIR/generated/ssl/cmd/kafkarestclass/create-tls-kafkarestclass-secret.sh
+source $BASE_DIR/generated/ssl/cmd/connect/create-tls-connect-secret.sh
+source $BASE_DIR/generated/ssl/cmd/schemaregistry/create-tls-schemaregistry-secret.sh
+source $BASE_DIR/generated/ssl/cmd/kafkarestproxy/create-tls-kafkarestproxy-secret.sh
+source $BASE_DIR/generated/ssl/cmd/controlcenter/create-tls-controlcenter-secret.sh
+
+source $BASE_DIR/generated/creds/oidc/cmd/create-oidc-controlcenter-secret.sh
+
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-controlcenter-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-kafkacontroller-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-kafkabroker-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-kafkarestclass-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-connect-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-schemaregistry-secret.sh
+source $BASE_DIR/generated/creds/oauth/cmd/create-sasl-oauth-txt-kafkarestproxy-secret.sh
